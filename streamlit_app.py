184eb4f0333cd53c0adba670788917590f475b09
hemaPriyakotu <129924695+hemaPriyakotu@users.noreply.github.com>
2023-04-05 17:08:06 +0530

    Update streamlit_app.py
    added inputs to check snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,10 @@ normalized_list=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(normalized_list)
 
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
