06f1a659d45d44ab00340ace1a4c98737b77046d
hemaPriyakotu <129924695+hemaPriyakotu@users.noreply.github.com>
2023-04-05 21:12:19 +0530

    Update streamlit_app.py
    fixed syntax issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-def insert_fruit_name():
+def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
         my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
         return "Thanks for adding" + new_fruit
@@ -79,6 +79,6 @@ def insert_fruit_name():
 add_my_fruit= streamlit.text_input('which fruit would you like to add?')
 if streamlit.button('Add Fruit to the list:'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-     back_from_function = insert_fruit_name(add_my_fruit)
+     back_from_function = insert_row_snowflake(add_my_fruit)
      streamlit.text(back_from_function)
     
