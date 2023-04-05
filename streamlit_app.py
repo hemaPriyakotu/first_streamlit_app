c00a55104b0895f000cee123190d8c948d6b2246
hemaPriyakotu <129924695+hemaPriyakotu@users.noreply.github.com>
2023-04-05 20:48:41 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 def get_fruitvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
     normalized_list=pandas.json_normalize(fruityvice_response.json())
-return fruityvice_normalized
+    return fruityvice_normalized
 
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
