87fe73d3793b2e5355093e86af432ece0b47455c
hemaPriyakotu <129924695+hemaPriyakotu@users.noreply.github.com>
2023-04-05 14:23:32 +0530

    Update streamlit_app.py
    added api request data to be pulled

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
@@ -14,3 +15,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
