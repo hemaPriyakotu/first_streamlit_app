f3ea253ee266378d4f46c67b0651406179f9e784
hemaPriyakotu <129924695+hemaPriyakotu@users.noreply.github.com>
2023-04-05 16:22:20 +0530

    Update streamlit_app.py
    added fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 normalized_list=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(normalized_list)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
